c68d9ffa135526725b76446971f78dd6cd1e699b
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-08-22 16:41:11 +0200

    [FEAT] seeding Animals Table w/ faker

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Animal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Person;
+
+
 
 class AnimalSeeder extends Seeder
 {
@@ -12,8 +17,20 @@ class AnimalSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 10; $i++){
+            $animal = new Animal();
+
+            $animal->nome = $faker->firstName();
+            $animal->specie = $faker->word();
+            $animal->data_di_nascita = $faker->date();
+            $animal->genere = $faker->word();
+            $animal->nome_proprietario= $faker->firstName();
+            $animal->cognome_proprietario= $faker->lastName();
+            $animal->note_aggiuntive= $faker->paragraph();
+    
+            $animal->save(); 
+        }
     }
 }
