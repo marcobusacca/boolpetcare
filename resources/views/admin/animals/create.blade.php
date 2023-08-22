044aef14b125d9e7e18d11cb96cbbdfcd5c94d55
andreathepa <andreadepa32@gmail.com>
2023-08-22 17:42:15 +0200

    [FEAT] create

--- resources/views/admin/animals/create.blade.php
+++ resources/views/admin/animals/create.blade.php
@@ -1,5 +1,66 @@
 @extends('layouts.admin')
 
 @section('content')
-    
+<div class="container mt-5">
+    <div class="row justify-content-between">
+        <div class="col-12">
+            <h1>Aggiungi una nuova bestia</h1>
+
+        </div>
+        <div class="col-12 mt-5">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="col-12">
+            <form action="{{ route('admin.animals.store')}}" method="POST">
+                @csrf
+                <div class="form-group mt-4">
+                    <label class="control-label">Nome animale</label>
+                    <input type="text" name="nome" id="nome" class="form-control" placeholder="Inserisci nome" value="{{ old ('nome')}}">
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Specie</label>
+                    <input type="text" name="specie" id="specie" class="form-control" placeholder="Inserisci specie">{{ old('specie')}}
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Data di nascita</label>
+                    <input type="text" name="data_di_nascita" id="data_di_nascita" class="form-control" placeholder="Inserisci data di nascita">{{ old('data_di_nascita')}}
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Genere</label>
+                    <input type="text" name="genere" id="genere" class="form-control" placeholder="Inserisci genere">{{ old('genere')}}
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Nome proprietario</label>
+                    <input type="text" name="nome_proprietario" id="nome_proprietario" class="form-control" placeholder="Inserisci nome proprietario">{{ old('nome_proprietario')}}
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Cognome proprietario</label>
+                    <input type="text" name="cognome_proprietario" id="cognome_proprietario" class="form-control" placeholder="Inserisci cognome proprietario">{{ old('cognome_proprietario')}}
+
+                </div>
+                <div class="form-group mt-4">
+                    <label class="control-label">Note aggiuntive</label>
+                    <textarea name="note_aggiuntive" id="note_aggiuntive" class="form-control" placeholder="Inserisci note aggiuntive">{{ old('note_aggiuntive')}}</textarea>
+
+                </div>
+                <div class="col-12 d-flex justify-content-center align-items-center my-5">
+                    <button class="btn btn-success fw-bold px-5" type="submit">CREA</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
