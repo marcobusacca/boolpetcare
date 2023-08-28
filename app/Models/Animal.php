f3c5eea6dfbb3f5f5ebbfc2a315d739d4b3e6c77
marcobusacca <marcobusacca01@gmail.com>
2023-08-28 16:09:45 +0200

    [FEAT] animal & vaccination models relation

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Vaccination;
+
 class Animal extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nome', 'specie', 'data_di_nascita', 'genere', 'nome_proprietario', 'cognome_proprietario', 'note_aggiuntive'];
 
+    public function vaccinations(){
+        return $this->hasMany(Vaccination::class);
+    }
 }
--- app/Models/Vaccination.php
+++ app/Models/Vaccination.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Animal;
+
 class Vaccination extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['animal_id', 'nome', 'data_di_vaccinazione', 'dosaggio', 'note_aggiuntive'];
+
+    public function animal(){
+        return $this->belongsTo(Animal::class);
+    }
 }
