8e736e69e1e5e18c0df31709eea0ccf3f812dc22
marcobusacca <marcobusacca01@gmail.com>
2023-08-28 16:04:40 +0200

    [FEAT] vaccinations table seeder

--- database/seeders/VaccinationSeeder.php
+++ database/seeders/VaccinationSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Vaccination;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class VaccinationSeeder extends Seeder
 {
@@ -12,8 +14,17 @@ class VaccinationSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 10; $i++){
+            $vaccination = new Vaccination();
+
+            $vaccination->nome = $faker->word();
+            $vaccination->data_di_vaccinazione = $faker->date();
+            $vaccination->dosaggio = $faker->randomDigit();
+            $vaccination->note_aggiuntive= $faker->paragraph();
+    
+            $vaccination->save(); 
+        }
     }
 }
