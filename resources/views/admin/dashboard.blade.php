c6de5c607e0af7388221647ad0752fc17f7c5570
andreathepa <andreadepa32@gmail.com>
2023-09-04 13:47:01 +0200

    modifica logged in

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('La tua Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,7 +17,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Hai effettuato il login correttamente!') }}
                 </div>
             </div>
         </div>
