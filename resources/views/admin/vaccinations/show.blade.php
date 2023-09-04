7854224c3a0c03364b79b55355a464c1585ed87e
marcobusacca <marcobusacca01@gmail.com>
2023-09-04 12:04:01 +0200

    [FIX] edit animal and show animal & vaccination

--- resources/views/admin/vaccinations/show.blade.php
+++ resources/views/admin/vaccinations/show.blade.php
@@ -30,6 +30,33 @@
                             <!-- Nome Content -->
                             <span class="py-2">{{ $vaccination->nome }}</span>
                         </div>
+                        <!-- Vaccinations Animals -->
+                        <div class="my-5 text-center">
+                            <div class="row justify-content-center">
+                                @if (count($vaccination->animals) == 0)
+                                    <span>Nessun Animale Vaccinato con il Vaccino "{{ $vaccination->nome }}"</span>
+                                @else
+                                    <!-- Vaccinations Animals Label -->
+                                    <label class="fw-bold">Animali Vaccinati con il Vaccino "{{ $vaccination->nome }}":</label>
+                                    <!-- List of Animals -->
+                                    @foreach ($vaccination->animals as $animal)
+                                        <div class="col-4 p-4">
+                                            <div class="card h-100">
+                                                <div class="card-body">
+                                                    <!-- Animal Nome -->
+                                                    <h5 class="card-title">{{ $animal->nome }}</h5>
+                                                    <!-- Animal Genere -->
+                                                    <p class="card-text">{{ $animal->genere }}</p>
+                                                </div>
+                                                <div class="card-footer py-3">
+                                                    <a href="{{ route('admin.animals.show', $animal) }}" class="btn btn-primary">Informazioni Animale</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
